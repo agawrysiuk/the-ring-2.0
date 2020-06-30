5e3ff242dc7e2d087ed293c5809ff7627fde0e13
dlrzz <arkad.gawry@gmail.com>
2020-06-30 22:09:34 +0200

    Added MessageCodes idea

---
+++ the-ring-2.0-client/src/main/java/pl/agawrysiuk/connection/MessageCode.java
@@ -0,0 +1,14 @@
+package pl.agawrysiuk.connection;
+
+/**
+ * Responsible for clear communication between the server and a client.
+ */
+
+import lombok.Getter;
+
+@Getter
+public enum MessageCode {
+    OK,
+    CHECK_CARDS,
+    MISSING
+}
