8000ea2ad19c9faac1366e1cd3cc3a0ee8d84a27
dlrzz <arkad.gawry@gmail.com>
2020-06-30 21:38:25 +0200

    Added Trigger -> Action idea for cards

---
+++ the-ring-2.0-client/src/main/java/pl/agawrysiuk/game/triggers/Trigger.java
@@ -0,0 +1,10 @@
+package pl.agawrysiuk.game.triggers;
+
+/**
+ * The game checks the Triggers and when it happens, calls the card's trigger method.
+ * Trigger's method responds with Action enum that needs to happen.
+ * The game looks for the specific class in Factory and calls it's method that needs to happen.
+ */
+
+public enum Trigger {
+}
