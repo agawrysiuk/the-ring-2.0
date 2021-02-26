c05271f8cd8cf25c9889f36aba0ab8c5a0bff738
dlrzz <arkad.gawry@gmail.com>
2021-02-26 11:36:38 +0100

    Added mirror view for playmat

--- the-ring-client-angular/src/app/shared/playmat/playmat.component.ts
+++ the-ring-client-angular/src/app/shared/playmat/playmat.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-playmat',
   templateUrl: './playmat.component.html',
   styleUrls: ['./playmat.component.scss']
 })
-export class PlaymatComponent implements OnInit {
+export class PlaymatComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @Input()
+  public topView = false;
+  @Input()
+  public rightSideView = false;
+
+  @ViewChild('playmat')
+  playmat: ElementRef;
+
+  @ViewChild('playmatLeft')
+  playmatLeft: ElementRef;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    if(this.topView) {
+      // this.playmat.nativeElement.style.transform = 'rotate(180deg)';
+    }
+
+    if(this.rightSideView) {
+      this.playmatLeft.nativeElement.style.order = '2';
+    }
   }
 
 }
