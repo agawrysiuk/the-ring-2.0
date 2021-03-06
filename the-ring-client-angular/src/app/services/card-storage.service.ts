2533930ced3f30383f7efd41178d6632b75f3d0b
dlrzz <arkad.gawry@gmail.com>
2021-03-06 12:40:32 +0100

    Added different card styles, different visibility on the board

--- the-ring-client-angular/src/app/services/card-storage.service.ts
+++ the-ring-client-angular/src/app/services/card-storage.service.ts
@@ -8,24 +8,37 @@ import {HttpClient} from "@angular/common/http";
 export class CardStorageService {
 
   private readonly JSON_CARDS_PLACEMENT = '../../assets/data/json/cards/';
-  private cardImages: { [key: string]: string; } = {};
+  private normalCardImages: { [key: string]: string; } = {};
+  private artCropImages: { [key: string]: string; } = {};
 
   constructor(private sanitizer: DomSanitizer,
               private http: HttpClient) {
   }
 
-  getCardImage(id: string) {
-    return this.cardImages[id]
-      ? new Promise((resolve) => resolve.apply(this.sanitize(this.cardImages[id]))) as Promise<string>
-      : this.loadFile(id).then(file => this.sanitize(file.image_uris.normal));
+  getCard(id: string) {
+    return this.loadCardFile(id);
+  }
+
+  getNormalImage(id: string) {
+    return this.getCardImage(id, 'normal', this.normalCardImages)
+  }
+
+  getArtCropImage(id: string) {
+    return this.getCardImage(id, 'art_crop', this.artCropImages)
+  }
+
+  private getCardImage(id: string, imageType: string, database: { [key: string]: string; }) {
+    return database[id]
+      ? new Promise((resolve) => resolve.apply(this.sanitize(database[id]))) as Promise<string>
+      : this.loadCardFile(id).then(file => this.sanitize(file.image_uris[imageType]));
   }
 
   private sanitize(base64Image: string) {
     return this.sanitizer.bypassSecurityTrustResourceUrl('data:image/jpg;base64,' + base64Image);
   }
 
-  private loadFile(id: string) {
-    console.log('Loading from file...')
+  private loadCardFile(id: string) {
+    console.log('Loading ' + id + ' from file...')
     // return this.http.get(this.JSON_CARDS_PLACEMENT + id + '.json').toPromise() as Promise<any>;
     return this.http.get(this.JSON_CARDS_PLACEMENT + '/594cb7dc-ea88-4909-ab40-1d40fecc9817.json').toPromise() as Promise<any>;
   }
--- the-ring-client-angular/src/app/shared/card/card.component.ts
+++ the-ring-client-angular/src/app/shared/card/card.component.ts
@@ -1,24 +1,44 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import {CardStorageService} from "../../services/card-storage.service";
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss', './card-playmat.component.scss']
 })
-export class CardComponent implements OnInit {
+export class CardComponent implements OnInit, AfterViewInit {
 
   @Input()
   public id: string;
   @Input()
   public styleNumber: number;
+  @Input()
+  public customStyles: any = null;
+  @Input()
+  public isPlaymatView: boolean = false;
+
+  @ViewChild('cardImg')
+  cardImg: ElementRef;
 
   public image;
+  private card: any;
 
   constructor(private storage: CardStorageService) {
+    this.storage.getCard(this.id).then(card => this.card = card);
   }
 
   ngOnInit(): void {
-    this.storage.getCardImage(this.id).then(res => this.image = res);
+    if(this.isPlaymatView) {
+      this.storage.getArtCropImage(this.id).then(res => this.image = res);
+    } else {
+      this.storage.getNormalImage(this.id).then(res => this.image = res);
+    }
+  }
+
+  ngAfterViewInit(): void {
+    if(this.customStyles) {
+      this.cardImg.nativeElement.style.width = this.customStyles.width;
+      this.cardImg.nativeElement.style.borderRadius = this.customStyles.borderRadius;
+    }
   }
 }
